a6dcc49afb75cd4110127a0f7e703e7ad5b188af
lcruiz <luiscarlosieti@gmail.com>
2020-10-06 16:31:56 -0500

    add /components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuxiliarHomeComponent } from './components/auxiliar-home/auxiliar-home.component';
+import { AuxiliarNavbarComponent } from './components/auxiliar-navbar/auxiliar-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuxiliarHomeComponent,
+    AuxiliarNavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/auxiliar-home/auxiliar-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auxiliar-home',
+  templateUrl: './auxiliar-home.component.html',
+  styleUrls: ['./auxiliar-home.component.css']
+})
+export class AuxiliarHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
