c19d400f08e7b692fcc06f0fa1a54500081fe34a
nato98 <natyorozco@unicauca.edu.co>
2020-10-08 17:16:27 -0500

    Plantilla de home y agregarProcedimiento para auxiliar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
 
 import { AppComponent } from './app.component';
-import { AuxiliarHomeComponent } from './components/auxiliar-home/auxiliar-home.component';
-import { AuxiliarNavbarComponent } from './components/auxiliar-navbar/auxiliar-navbar.component';
-import { PacienteComponent } from './components/paciente/paciente.component';
-import { ReactiveFormsModule } from '@angular/forms';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { PacienteComponent } from './paciente/paciente.component';
+
+import {
+  AgmCoreModule
+} from '@agm/core';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AuxiliarProgramarProcedimientoComponent } from './auxiliar-programar-procedimiento/auxiliar-programar-procedimiento.component';
 
 @NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ComponentsModule,
+    RouterModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
+    })
+  ],
   declarations: [
     AppComponent,
-    AuxiliarHomeComponent,
-    AuxiliarNavbarComponent,
-    PacienteComponent
-  ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule
+    AdminLayoutComponent,
+    AuxiliarProgramarProcedimientoComponent,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
